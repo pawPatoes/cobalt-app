c63c74299ce449d32ca483c91698d64c49715dd8
pawPatoes <pawpatoes@example.com>
2026-08-13 09:18:27 +0400

    Correctly update env.ts with wolfy.live fallback

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -18,7 +18,7 @@ const variables = {
     HOST: getEnv('HOST'),
     PLAUSIBLE_HOST: getEnv('PLAUSIBLE_HOST'),
     PLAUSIBLE_ENABLED: getEnv('HOST') && getEnv('PLAUSIBLE_HOST'),
-    DEFAULT_API: getEnv('DEFAULT_API'),
+    DEFAULT_API: getEnv('DEFAULT_API') || "https://cobalt-alpha.wolfy.live",
     ENABLE_WEBCODECS: getEnvBool('ENABLE_WEBCODECS'),
     ENABLE_DEPRECATED_YOUTUBE_HLS: getEnvBool('ENABLE_DEPRECATED_YOUTUBE_HLS'),
 }
