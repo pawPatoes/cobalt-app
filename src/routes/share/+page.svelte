2ec52229f901727dd7b5145123be8abfc82b8f97
pawPatoes <264230241+pawPatoes@users.noreply.github.com>
2026-08-14 15:46:49 +0400

    Update +page.svelte

--- src/routes/share/+page.svelte
+++ src/routes/share/+page.svelte
@@ -1,136 +1,48 @@
-<script lang="ts">
+<script>
     import { t } from "$lib/i18n/translations";
-    import { createRemuxPipeline } from "$lib/task-manager/queue";
-
-    import DropReceiver from "$components/misc/DropReceiver.svelte";
-    import FileReceiver from "$components/misc/FileReceiver.svelte";
-    import BulletExplain from "$components/misc/BulletExplain.svelte";
-
-    import IconRepeat from "@tabler/icons-svelte/IconRepeat.svelte";
-    import IconDevices from "@tabler/icons-svelte/IconDevices.svelte";
-    import IconInfoCircle from "@tabler/icons-svelte/IconInfoCircle.svelte";
-
-    let draggedOver = false;
-    let files: FileList | undefined;
-
-    const remux = async () => {
-        if (!files) return;
-
-        for (let i = 0; i < files?.length; i++) {
-            const type = files[i].type;
-            // TODO: stricter type limits?
-            if (type.startsWith("video/") || type.startsWith("audio/")) {
-                createRemuxPipeline(files[i]);
-            }
-        }
-
-        files = undefined;
-    };
+    import Meowbalt from "$components/misc/Meowbalt.svelte";
 </script>
 
 <svelte:head>
-    <title>{$t("tabs.remux")} ~ {$t("general.cobalt")}</title>
-    <meta
-        property="og:title"
-        content="{$t('tabs.remux')} ~ {$t('general.cobalt')}"
-    />
+    <title>{$t("general.cobalt")} - {$t("tabs.share")}</title>
+    <meta property="og:title" content={$t("tabs.share")} />
 </svelte:head>
 
-<DropReceiver bind:files bind:draggedOver onDrop={remux} id="remux-container">
-    <div id="remux-open" tabindex="-1" data-first-focus>
-        <div id="remux-receiver">
-            <FileReceiver
-                bind:draggedOver
-                bind:files
-                onImport={remux}
-                acceptTypes={["video/*", "audio/*"]}
-                acceptExtensions={[
-                    "mp4",
-                    "webm",
-                    "mp3",
-                    "ogg",
-                    "opus",
-                    "wav",
-                    "m4a",
-                ]}
-            />
-        </div>
-
-        <div id="remux-bullets">
-            <BulletExplain
-                title={$t("remux.bullet.purpose.title")}
-                description={$t("remux.bullet.purpose.description")}
-                icon={IconRepeat}
-            />
-
-            <BulletExplain
-                title={$t("remux.bullet.explainer.title")}
-                description={$t("remux.bullet.explainer.description")}
-                icon={IconInfoCircle}
-            />
-
-            <BulletExplain
-                title={$t("remux.bullet.privacy.title")}
-                description={$t("remux.bullet.privacy.description")}
-                icon={IconDevices}
-            />
-        </div>
-    </div>
-</DropReceiver>
+<div id="cobalt-share-container" class="center-column-container" tabindex="-1" data-first-focus>
+    <main id="cobalt-share">
+        <Meowbalt emotion="think" />
+        <h1>{$t("tabs.share")}</h1>
+        <p>{$t("share.description")}</p>
+    </main>
+</div>
 
 <style>
-    :global(#remux-container) {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+    #cobalt-share-container {
+        padding: var(--padding);
+        overflow: hidden;
         width: 100%;
-    }
-
-    #remux-open {
+        height: 100%;
         display: flex;
-        flex-direction: row;
         justify-content: center;
         align-items: center;
+    }
+
+    #cobalt-share {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        gap: 15px;
         text-align: center;
-        gap: 48px;
     }
 
-    #remux-receiver {
-        max-width: 450px;
-        display: flex;
-        flex-direction: column;
-        gap: var(--padding);
+    h1 {
+        font-size: 24px;
+        color: var(--secondary);
     }
 
-    #remux-bullets {
-        display: flex;
-        flex-direction: column;
-        gap: 18px;
-        max-width: 450px;
-    }
-
-    @media screen and (max-width: 920px) {
-        #remux-open {
-            flex-direction: column;
-            gap: var(--padding);
-        }
-
-        #remux-bullets {
-            padding: var(--padding);
-        }
-    }
-
-    @media screen and (max-width: 535px) {
-        #remux-bullets {
-            gap: var(--padding);
-        }
-    }
-
-    @media screen and (max-height: 750px) and (max-width: 535px) {
-        :global(#remux-container:not(.processing)) {
-            justify-content: start;
-            align-items: start;
-            padding-top: var(--padding);
-        }
+    p {
+        color: var(--gray);
     }
 </style>
